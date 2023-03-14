49b68ff4d09f021408579be5df0ab6ffabc63f7c
Roma1717 <rzavtonev@gmail.com>
2023-03-14 18:28:10 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //Console.Write("Введите кол-во элементов в массиве: ");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] array = new int[n];
+            //Random rand = new Random();
+            //for (int i = 0; i < n; i++)
+            //{
+            //    array[i] = rand.Next(1, 81);
+            //}
+            //Console.WriteLine("Исходный массив:");
+            //foreach (int elem in array)
+            //{
+            //    Console.Write("{0} ", elem);
+            //}
+            //Console.WriteLine();
+            //int max = array[0];
+            //int min = array[0];
+            //int maxIndex = 0;
+            //int minIndex = 0;
+            //for (int i = 1; i < n; i++)
+            //{
+            //    if (array[i] > max)
+            //    {
+            //        max = array[i];
+            //        maxIndex = i;
+            //    }
+            //    if (array[i] < min)
+            //    {
+            //        min = array[i];
+            //        minIndex = i;
+            //    }
+            //}
+
+            //Console.WriteLine("Макс. элемент: {0}, Мин. элемент: {1}", max, min);
+            //int temp = array[maxIndex];
+            //array[maxIndex] = array[minIndex];
+            //array[minIndex] = temp;
+
+            //Console.WriteLine("После обмена:");
+            //foreach (int elem in array)
+            //{
+            //    Console.Write("{0} ", elem);
+            //}
+            //Console.ReadKey();
+            //Console.WriteLine();
+
+            //Задача 2 
+            //int[] array = new int[12];
+            //Console.WriteLine("Введите кол-во элементов массива:");
+            //for (int i = 0; i < 12; i++)
+            //{
+            //    Console.Write($"Элемент {i + 1}: ");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+            //Array.Sort(array);
+            //Array.Reverse(array);
+            //Console.WriteLine("В порядке убывания: ");
+            //foreach (double element in array)
+            //{
+            //    Console.Write(element + " ");
+            //}
+            //int max = array[0];
+            //int min = array[11];
+            //int sum = max + min;
+            //Console.WriteLine();
+            //Console.WriteLine($"Максимальный элемент: {max}");
+            //Console.WriteLine($"Минимальный элемент: {min}");
+            //Console.WriteLine($"Сумма: {sum}");
+            //Console.ReadKey();
+
+            //Задача 3        
+            //double[] array = new double[14];
+            //Console.WriteLine("Введите 14 элементов массива:");
+            //for(int i = 0; i < 14; i++)
+            //{
+            //     Console.Write($"Элемент {i + 1}: ");
+            //     array[i] = double.Parse(Console.ReadLine());
+            //}
+            //Array.Sort(array, 0, 7);
+            //Array.Sort(array, 7, 7);
+            //Array.Reverse(array, 7, 7);
+            //Console.WriteLine("В порядке возрастания от 1 до 7,а 8-14 в порядке убывания:");
+            //foreach(double element in array)
+            //{
+            //    Console.Write(element + " ");
+            //}
+            //Console.ReadKey();
+            
+        }
+
+    }
+}
+
+
+        
+    
+
